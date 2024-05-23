9728db613fab7a2242aa945949a0c67b864b7c69
Nickwildez <wildeznick@gmail.com>
2024-05-23 11:36:12 +0700

    update tampilan

---
+++ script.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 1000,
+    });
+    document.body.classList.add('loaded');
+})
+
+function tutup() {
+    const tutupNav = document.querySelector('.nav-content');
+    tutupNav.classList.toggle('closed');
+
+    const buttonNav = document.querySelector('#toggleNav');
+    buttonNav.classList.toggle('closed');
+}
+
+function openAllContent() {
+    const homePembukaan = document.querySelector('.home');
+    const aboutContent = document.querySelector('.about');
+    const CardInfo = document.querySelector('.card');
+    const footerInfo = document.querySelector('footer');
+    const OpenHeader = document.querySelector('header');
+
+    homePembukaan.style.display = "none";
+    aboutContent.classList.add('openContent');
+    CardInfo.classList.add('openContent');
+    footerInfo.classList.add('openContent');
+    OpenHeader.classList.add('visible');
+}
+
+const classSection = document.querySelectorAll('section');
+const headerNavA = document.querySelectorAll('header nav a');
+window.onscroll =  () => {
+    classSection.forEach(sec => {
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 150;
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
+        if(top > offset && top < offset + height) {
+            headerNavA.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+            })
+        }
+    })
+}
+
+function OpenCallDEV() {
+    const secCallDEV = document.querySelector('.sec-callDev');
+    secCallDEV.classList.add('opencallDEV');
+}
+
+function removeCallDev() {
+    const secCallDEV = document.querySelector('.sec-callDev');
+    secCallDEV.classList.remove('opencallDEV');
+}
